219db4464395f0857b5064e1ab1c28eb4d88001f
AndriiTyschuk <andrii.tyschuk@ithoot.com>
2024-11-26 09:55:15 +0200

    fix: add headless for launched browser

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -6,10 +6,8 @@ export class CompanyService {
     async getCompanies(): Promise<string[]>{
 
         const browser = await puppeteer.launch(
-
             //can't work for Andrii without that
-            // { headless: false }
-             
+            { headless: false }
         )
         const page = await browser.newPage()
 
@@ -17,23 +15,24 @@ export class CompanyService {
 
         try {
             await page.goto('https://clutch.co/developers')
-            await page.goto('https://clutch.co/developers/ecommerce')
+            // await page.goto('https://clutch.co/developers/ecommerce')
             await page.setViewport({
                  width: 1920,
                 height: 1080,
             })
-
-            console.log(await page.content());
+            // console.log(await page.content());
 
             await page.waitForSelector('.providers__list');
 
             companies = await page.$$eval(
                 '.providers__list', 
-                list => list.map(elem => elem.innerHTML)
+                list => list.map(elem => 
+                    elem.innerHTML
+                )
             )
 
         } catch (error){
-            console.log('Error: ', error)
+            console.log('Scrapping error: ', error)
         } finally{
             await browser.close()
             return companies
