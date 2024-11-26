2fa4f745dd093bc32d6663f35c73a1215b195339
AndriiTyschuk <andrii.tyschuk@ithoot.com>
2024-11-26 10:04:18 +0200

    add const for links

---
+++ src/routeConstants.ts
@@ -0,0 +1,22 @@
+export default () => ({
+
+    //urls
+    baseurl: 'clutch.co',
+    dev: 'developers',
+    ecom: 'ecommerce',
+
+    //parameters
+    focus_areas: 'focus_areas',
+    dotnet: 'field_pp-fw_dot_net',
+    mark: 4,
+    reviewed: true,
+
+    // location
+    britain: '',
+    denmark: 'dk',
+    france: 'fr',
+    netherlands: 'nl',
+    norway: 'no',
+    sweden: 'se',
+    ukraine: 'uk',
+})
